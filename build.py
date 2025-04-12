27927c706a23d57e613093e4028c6253d81d13bc
Vladas Zakrevskis <146100@gmail.com>
2025-04-13 02:27:42 +0300

    linux back

--- build.py
+++ build.py
@@ -28,8 +28,8 @@ def copy_artifacts():
     targets = {
         "x86_64-linux": "target/x86_64-unknown-linux-gnu/release/laptop_exporter",
         "aarch64-linux": "target/aarch64-unknown-linux-gnu/release/laptop_exporter",
-        "x86_64-macos": "target/x86_64-apple-darwin/release/laptop_exporter",
-        "aarch64-macos": "target/aarch64-apple-darwin/release/laptop_exporter",
+        # "x86_64-macos": "target/x86_64-apple-darwin/release/laptop_exporter",
+        # "aarch64-macos": "target/aarch64-apple-darwin/release/laptop_exporter",
     }
 
     for arch, path in targets.items():
@@ -65,7 +65,7 @@ if __name__ == "__main__":
 
     build_cross("x86_64-unknown-linux-gnu")
     build_cross("aarch64-unknown-linux-gnu")
-    build("x86_64-apple-darwin")
-    build("aarch64-apple-darwin")
+    # build("x86_64-apple-darwin")
+    # build("aarch64-apple-darwin")
 
     copy_artifacts()
